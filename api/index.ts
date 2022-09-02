3474e5b92f5bfd4b77c7ec721fe56c79b5fdd288
M-urushibara <s203128@stn.nagaokaut.ac.jp>
2022-09-02 19:29:36 +0900

    aaa

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import articleRoutes  from './routes/articles';
-
+import cors from 'cors';
 
 const app = express();
 app.use(express.static('views'));
 app.use(bodyParser.json());
-app.use('/articles', articleRoutes);
+app.use(cors({
+    origin: 'http://localhost:3000', //アクセス許可するオリジン
+    credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+    optionsSuccessStatus: 200 //レスポンスstatusを200に設定
+}));
 
+
+
+app.use('/articles', articleRoutes);
 
 
 app.listen(3001, () => console.log('Server is running'));
