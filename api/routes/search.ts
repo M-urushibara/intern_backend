7c260a39242d567baa79a509a9b6fc7bebd2388d
M-urushibara <s203128@stn.nagaokaut.ac.jp>
2022-09-03 09:14:07 +0900

    検索api作成

--- api/routes/search.ts
+++ api/routes/search.ts
@@ -1,32 +1,32 @@
-import { Router, Request, Response } from 'express';
-import { findrecommendArticles, findArticle } from '../usecases/articles';
+// import { Router, Request, Response } from 'express';
+// import { findrecommendArticles, findArticle } from '../usecases/articles';
 
 
-const router = Router();
+// const router = Router();
 
-router.get(
-    '/timeline/:category',
-    async (req:Request, res:Response) => {
-        const recommendArticles = await findrecommendArticles(req.params.category);
-        res.json(recommendArticles)
-    }
-);
+// router.get(
+//     '/timeline/:category',
+//     async (req:Request, res:Response) => {
+//         const recommendArticles = await findrecommendArticles(req.params.category);
+//         res.json(recommendArticles)
+//     }
+// );
 
-router.get(
-    '/:id',
-    async (req:Request, res:Response) => {
-        const article = await findArticle(req.params.id);
-        res.json(article)
-    }
-)
+// router.get(
+//     '/:id',
+//     async (req:Request, res:Response) => {
+//         const article = await findArticle(req.params.id);
+//         res.json(article)
+//     }
+// )
 
-router.get(
-    '/:id',
-    async (req:Request, res:Response) => {
-        const article = await findArticle(req.params.id);
-        res.json(article)
-    }
-)
+// router.get(
+//     '/:id',
+//     async (req:Request, res:Response) => {
+//         const article = await findArticle(req.params.id);
+//         res.json(article)
+//     }
+// )
 
 
-export default router;
+// export default router;
